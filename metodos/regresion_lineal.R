9d76947b71f8238651f1dd3398adcca243d6e585
violetr <violetr@users.noreply.github.com>
2017-03-20 12:23:44 -0300

    Add files via upload

---
+++ metodos/regresion_lineal.R
@@ -0,0 +1,160 @@
+# METODOS reglineal
+
+library(glmnet)
+library(matrixcalc)
+
+# regresion lineal:
+#   adaptive lasso cv: 
+#   thresholded lasso cv:
+#   stability selection:
+#   
+
+#adaptive lasso
+
+cv.adaptive.lasso<-function(datos,y,nfolds,termino.indep=TRUE){
+  
+  modelolasso<-cv.glmnet(x=datos,y=y,alpha=1,nfolds = 10,intercept=FALSE)
+  #obtengo coeficientes para el lambda con menos error
+  coeflasso=coef(modelolasso,s="lambda.min")
+  #saco el intercept
+  coeflasso=coeflasso[2:1001]
+  
+  pesos=as.integer(!coeflasso==0)*1/abs(coeflasso)
+  pesos[is.na(pesos)]=10000
+  
+  modeloadaplasso<-cv.glmnet(x=datos,y=y,alpha=1,nfolds = nfolds,penalty.factor = pesos,intercept=termino.indep)
+  
+  #hago el mismo analisis que en los dos metodos anteriores
+  
+  coefadap=coef(modeloadaplasso,s="lambda.min")
+  if(!intercept){
+    coefadap=coefadap[2:1001]
+  }
+  
+  return(coefadap)
+
+}
+
+
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%#
+# FUNCION
+# threslasso(datosexplicativos,respuesta,tau,lambda):
+# metodo thresholded lasso para regresion
+
+# INPUT:
+# datosexplicativos: matriz con datos explicativos dim=nx(p-1)
+# respuesta: vector respuesta length=n
+# tau: valor critico de corte
+# lambda: valor de regularizacion
+
+# OUTPUT:
+# sthres: variables que fueron refiteadas
+# coefthres: coeficientes de la regresion  
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%#
+threslasso<-function(datosexplicativos,respuesta,tau,lambda){
+  
+  modelolasso<-glmnet(x=datosexplicativos,y=respuesta,alpha=1,lambda = lambda)
+  coeflasso=as.matrix(modelolasso$beta)[,1]
+  
+  coefthreslasso=coeflasso*as.integer(abs(coeflasso)>tau)
+  seleccionada=(coefthreslasso !=0)
+  
+  sthres=obtenerindices(seleccionada)
+  
+  if(!(length(sthres) && sthres==0)){
+    
+    coefthresrefit=as.vector(t(matrix.inverse(t(datosexplicativos[,sthres])%*%t(t(datosexplicativos[,sthres])))%*%t(datosexplicativos[,sthres])%*%respuesta))
+    for(k in 1:length(sthres)){
+      coefthreslasso[sthres[k]]=coefthresrefit[k]  
+    }
+  }
+  
+  return(list(sthres=sthres,coefthres=coefthreslasso))
+}
+
+
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%#
+# FUNCION
+# cv.tau.threslasso(datosexplicativos,respuesta,tau,lambda):
+# metodo thresholded lasso para regresion 
+
+# INPUT:
+# beta.init: vector de coeficientes de la estimacion
+# inicial con lasso
+# lambda.best: lambda.cv de la estimacion inicial
+# con lasso
+# datosexplicativos: matriz con datos explicativos dim=nx(p-1)
+# respuesta: vector respuesta length=n
+# ntau: cantidad de 
+
+# OUTPUT:
+# sthres: variables que fueron refiteadas
+# coefthres: coeficientes de la regresion  
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%#
+
+
+cv.threslasso<-function(coeflasso,datosexplicativos,respuesta,tau){
+  
+  coefthreslasso=coeflasso*as.integer(abs(coeflasso)>tau)
+  seleccionada=(coefthreslasso !=0)
+  
+  sthres=obtenerindices(seleccionada)
+  
+  
+  if(!(length(sthres) && sthres==0)){
+    
+    coefthresrefit=lm(respuesta ~ . - 1,as.data.frame(datosexplicativos[,sthres]))$coefficients
+    
+    for(k in 1:length(sthres)){
+      coefthreslasso[sthres[k]]=coefthresrefit[k]  
+    }
+    
+  }
+  
+  return(list(sthres=sthres,coefthres=coefthreslasso))
+}
+
+
+cv.tau.threslasso<-function(beta.init,lambda.best,folds,datosexplicativos,respuesta,ntau=100){
+  
+  ngrupos=length(folds)  
+  conjtau=seq(0,1,length.out = ntau)  
+  mse=rep(0,ntau)
+  
+  #recorro todos los grupos
+  for(k in 1:ngrupos){
+    
+    datostrain=datosexplicativos[-folds[[k]],]
+    datostest=datosexplicativos[folds[[k]],]  
+    respuestatrain=respuesta[-folds[[k]]]
+    respuestatest=respuesta[folds[[k]]]
+    
+    ntrain=nrow(datostrain)
+    ntest=nrow(datostest)
+    
+    #recorro todos los folds
+    for(j in 1:ntau){
+      
+      tau=conjtau[j]
+      coefthreslassorefit=cv.threslasso(beta.init,datostrain,respuestatrain,tau)$coefthres      
+      predichos=predecir(datostest,coefthreslassorefit)
+      
+      mse[j]=mse[j]+mean((predichos-respuestatest)^2)
+    }
+    
+  }
+  tauganador=conjtau[which.min(mse)]
+  
+  #calculo coeficientes con el tau ganador
+  coefthreslassorefitgana=threslasso(beta.init,datostrain,respuestatrain,tauganador)$coefthres
+  
+  return(list(coef=coefthreslassorefitgana,tau=tauganador,mse=mse))
+}
+
+
+
+
+
+
+
+
---
+++ metodos/seleccion_modelo_binario.R
@@ -0,0 +1,103 @@
+# estimacion modelo binario
+
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%#
+
+# FUNCION
+# nodewiselogreg(datos,and=FALSE,metodo="CV")
+
+# estima el grafo del modelo binario (ising) con el 
+# metodo nodewise logistic regression que consiste en 
+# realizar regresiones log\'isticas para estimar
+# el vecindario de cada nodo
+
+# INPUT:
+# datos: Matriz de datos dim=nxp
+# and: Regla para establecer el vecindario (AND o OR)
+# metodo: metodo para la seleccion del parametro
+#         de penalizacion lambda. opciones:
+#         c("CV","wain","EBIC")
+
+# OUTPUT:
+# matriz de parametros estimados Theta
+# matriz con estructura del grafo con booleans
+
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%#
+
+
+#muestras=list()
+#muestras[[i]]
+#nlambda=50
+
+nodewiselogreg<-function(datos,and=FALSE,metodo="CV",gamma=0.25,nlambda=100){
+  n=nrow(datos)
+  p=ncol(datos)
+  
+  coeficientes=matrix(rep(1,p*p),p,p)
+  
+  for(i in 1:p){
+    print(i)
+    datosexplicativos=datos[,-i]
+    if(metodo=="CV"){
+      model=cv.glmnet(datosexplicativos,datos[,i],family="binomial",nfolds=5)
+      coeff=coef(model,s="lambda.min")
+    }
+    if(metodo=="EBIC"){
+      model=glmnet(datosexplicativos,datos[,i],family="binomial",nlambda = nlambda)
+      nlambda=length(model$lambda)
+      EBICm=rep(EBIC(n,p,0,0.25,p-1),nlambda)
+      
+      for(kiter in 1:nlambda){
+        print(kiter)
+        variablesseleccionadas=obtenerindices(model$beta[,kiter]!=0)
+        if((length(variablesseleccionadas)>1) | (length(variablesseleccionadas)==1 & variablesseleccionadas[1]!=0)){
+          
+          subdatos=as.data.frame(datosexplicativos[,variablesseleccionadas])
+          modellogl=glm(datos[,i] ~ ., family="binomial", data=subdatos)
+          logl=logLik(modellogl)
+          EBICm[kiter]=EBIC(n,p,logl,gamma,length(variablesseleccionadas))
+        }
+        
+      }
+      
+      lambdaselec=model$lambda[which.min(EBICm)]
+      
+      coeff=coef(model,s=lambdaselec)
+      
+    }
+    if(metodo=="stability"){
+      lambda=17*sqrt(log(p)/n)
+    }
+    
+    for(j in 1:p){
+      #esto lo tengo que separar porque en el vector de coeficientes tengo un lugar menos
+      if(j<i){
+        coeficientes[i,j]=coeff[j+1]
+      }
+      if(i<j){
+        coeficientes[i,j]=coeff[j]
+      }
+      if(i==j){
+        coeficientes[i,j]=coeff[1]
+      }
+    }
+  }
+  AMi=coeficientes !=0
+  
+  #los de la diagonal quedan en TRUE
+  AM=AMi
+  for(i in 1:p){
+    for(j in 1:p){
+      if(i != j){
+        if(and==TRUE){
+          AM[i,j]=AMi[i,j] && AMi[j,i]
+        }else{
+          AM[i,j]=AMi[i,j] || AMi[j,i]
+        }
+      }
+    }
+  }
+  
+  cnoceros=log(sum(AM==TRUE))
+  
+  return(AM)
+}
