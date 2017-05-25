38eb566f0046da545bcd8378d85cadde6975222c
Violeta Roizman <violeroizman@gmail.com>
2017-05-24 23:08:35 -0300

    diputados cambio stiempo, colores y colores aristas para presentacion

--- analisis/4_4_2_dipus.R
+++ analisis/4_4_2_dipus.R
@@ -50,23 +50,36 @@ dameListaBloques <- function(conjDiputadoId, datavotos){
 }
 
 contarFaltas <- function(columna) {
-  return(sum(columna == 3))
+  return(sum(is.na(columna)))
 }
 
+
 ############################datos##############################
+
+#as.Date(asuntos$fecha,format='%m/%d/%Y')<as.Date('05/12/2002',format='%m/%d/%Y')
+#  as.Date("2007-06-22")
+
 
 votos <- read.csv('./datos/votaciones-diputados.csv')
 diputados <- read.csv('./datos/diputados.csv')
 asuntos <- read.csv('./datos/asuntos-diputados.csv')
+asuntos$fecha
+
+#asuntosselec = asuntos[asuntos$ano >= 2013 & asuntos$ano <= 2015, 1]
+esta.en.tiempo = as.Date(asuntos$fecha,format='%m/%d/%Y')>as.Date('10/12/2013',format='%m/%d/%Y') & as.Date(asuntos$fecha,format='%m/%d/%Y')<as.Date('9/12/2015',format='%m/%d/%Y')
+asuntosselec = asuntos[esta.en.tiempo, 1]
 
 
-asuntosselec = asuntos[asuntos$ano >= 2013 & asuntos$ano <= 2015, 1]
+
+
 length(asuntosselec)
 # me quedo con los votos corresp a los asuntos seleccionados cuyos bloques pertenecen
 # a los mas importantes y que no sean el diputado 5 que no esta identificado
-votosselec = votos[votos$asuntoId  %in% asuntos[asuntos$ano >= 2013 & asuntos$ano <= 2015, 1] & votos$bloqueId %in% c(66, 67, 64, 109, 136, 179, 17, 18, 19, 172) & votos$diputadoId != 5, ]
+votosselec = votos[votos$asuntoId  %in% asuntos[esta.en.tiempo, 1] & votos$bloqueId %in% c(66, 67, 64, 109, 136, 179, 17, 18, 19, 172) & votos$diputadoId != 5, ]
 
 diputadosselec = unique(votosselec$diputadoId)
+#borro a un par que renunciaron y a los que los reemplazaron
+diputadosselec <- setdiff(diputadosselec, c(296,912,1037,753))
 #dameListaApellidos(diputadosselec,diputados)
 #verificar carac
 #length(diputadosselec)
@@ -90,26 +103,26 @@ for (j in 1:length(diputadosselec)) {
   indicesAfirmativos = dameListaIndicesAsuntos(votosafirmativos, asuntosselec)
   indicesAusencias = dameListaIndicesAsuntos(ausencias, asuntosselec)
   nueva.columna[indicesAfirmativos] = 1
-  nueva.columna[indicesAusencias] = 3
+  nueva.columna[indicesAusencias] = NA
   mis.datitos[ , j] = nueva.columna
 }
 
-nrosvar = obtenerindices(apply(mis.datitos, 2, contarFaltas) < nrow(mis.datitos) /4 & apply(mis.datitos, 2, sum) > 8 & nrow(mis.datitos) - apply(mis.datitos, 2, sum) > 8)
+nrosvar = obtenerindices(apply(mis.datitos, 2, contarFaltas) < nrow(mis.datitos) /4 & apply(mis.datitos, 2, sum,na.rm=TRUE) > 8 & (nrow(mis.datitos) - apply(mis.datitos, 2, sum,na.rm=TRUE)) > 8)
 
 
 nuevadata = mis.datitos[ , nrosvar]
-nuevadata[nuevadata == 3] = 0
+nuevadata[is.na(nuevadata)] = 0
 
 dim(nuevadata)
 nombrescolumnas = dameListaApellidos(diputadosselec[nrosvar], diputados)
 colnames(nuevadata) = nombrescolumnas
 
-
 bloques = as.character(dameListaBloques(diputadosselec[nrosvar], votosselec))
-
 #############################################################
 
-modelo = IsingFit(nuevadata, gamma = 0.75)
+modelo = IsingFit(nuevadata, gamma = 0.18)
+
+names(modelo)
 
 plot(modelo)
 
@@ -118,19 +131,23 @@ for (i in 1:dim(ady)[1]) {
   ady[i, i] = FALSE
 }
 
+negativos<-1*(modelo$weiadj<0)
 
 
 # borro los aislados
 sinisolatedd = ady[apply(ady, 2, sum) != 0, apply(ady, 2, sum) != 0]
 dim(sinisolatedd)
 bloquessiniso = bloques[apply(ady, 2, sum) != 0]
-
+negativossiniso <- negativos[apply(ady, 2, sum) != 0,apply(ady, 2, sum) != 0]
 
 #####################colores#################################
 
-coloresh <- c("firebrick2", "dodgerblue2", "gold", "firebrick1", "deepskyblue1", "gold")
+#coloresh <- c("firebrick2", "dodgerblue2", "gold", "firebrick1", "deepskyblue1", "gold")
+coloresh <- c("paleturquoise1","indianred1", "skyblue", "gold","gold")
+
 names(coloresh) = unique(bloques)
 
+#indianred1
 # 172 ucr firebrick
 # 67  FPV dodgerblue2
 # 136 pro gold
@@ -144,5 +161,7 @@ netd <- network::network(sinisolatedd, directed = FALSE)
 
 netd %v% "bloque" <- bloquessiniso
 
-ggnet2(netd, size = 8, label = TRUE, alpha = 1, label.size = 1.5, color = coloresh[netd %v% "bloque"], edge.color = "lightgrey")
+netd %e% "negativo" <- negativossiniso
+
+ggnet2(netd, size = 7, label = TRUE, alpha = 1, label.size = 2, color = coloresh[netd %v% "bloque"], edge.color = ifelse(netd %e% "negativo"==1,"firebrick1","forestgreen"))
 
